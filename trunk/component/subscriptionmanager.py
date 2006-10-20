4e68f301afb57a2883854e90a1ec202bed06f3f5
bickfordb <bickfordb@64667f48-3f20-0410-b4e2-f79fee9b7bb1>
2006-10-20 15:35:17 +0000

    simple subscriptionmanager

---
+++ trunk/component/subscriptionmanager.py
@@ -0,0 +1,73 @@
+#!env python
+"simpler than zope.interface.adapter.AdapterRegistry()" 
+import itertools
+from zope.interface import Interface, implements
+import types
+
+class InterfaceError(ValueError): 
+	pass
+
+# marker type
+Any = types.ClassType('Any', (object, ), {})() 
+
+class SubscriptionManager(object):
+	"i manage interface->sender->listeners"
+
+	def __init__(self): 
+		self.subscriptions = {}
+ 
+	def subscribe(self, interface, receiver, sender = Any):
+		"subscribe receiver with interface and optional sender"
+		if not interface.providedBy(receiver):
+			raise InterfaceError(receiver)
+
+		subscribers = self.subscriptions.setdefault((interface, sender), [])
+		if receiver not in subscribers: 
+			subscribers.append(receiver) 
+
+	def subscribers(self, interface, sender = Any):
+		"iterate over the subscribers for an interface and optional sender"
+		def _subscribers():
+			iter1 = iter(self.subscriptions.get((interface, sender), ()))
+			if sender != Any:
+				return itertools.chain(iter1, self.subscriptions.get((interface, Any), ())) 
+			else: 
+				return iter1
+			
+		_seen = set() 
+		def seen(x): 
+			if x not in _seen: 
+				_seen.add(x)
+				return True
+			return False
+	
+		return itertools.ifilter(seen, _subscribers())
+
+if __name__ == "__main__":
+	import unittest
+	class TestSubscription(unittest.TestCase):
+		def test_subscription(self):
+			"only implementing interfaces are allowed to be receivers"
+			x = SubscriptionManager()
+			class A(Interface): pass
+
+			class B(object):
+				implements(A)
+
+			class C(object):
+				pass
+
+			x.subscribe(A, B())
+			
+			self.assertRaises(InterfaceError, x.subscribe, A, C())
+
+			"test membership"	
+			x = SubscriptionManager()
+			z = B() 
+			x.subscribe(A, z)
+			self.assert_(z in x.subscribers(A))
+			y = object()
+			x.subscribe(A, z, y)
+			self.assertEquals(list(x.subscribers(A, y)), [z])
+ 	
+	unittest.main() 	
