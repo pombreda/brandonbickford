909d80d671b626a0c6c449cd73ed7665fec8aa4c
bickfordb <bickfordb@64667f48-3f20-0410-b4e2-f79fee9b7bb1>
2007-01-10 00:22:57 +0000

    latest

--- haskell/runtemplate.hs
+++ haskell/runtemplate.hs
@@ -2,9 +2,8 @@ module Main where
 import Data.Maybe
 import Ctemplate
 
-import qualified Data.Map
+import qualified Data.Map as Map
 import Data.Map (fromList, empty, singleton)
-
 main = do
          run "hi there"
          run "{{hello:escape_javascript}}"
@@ -13,10 +12,34 @@ main = do
          run "{{#hello}}{{#x}}{{/x}}{{/hello}}"
          run ""
          run "{{>x}}"
-         let y = runOrError "{{#results}}{{name}}, {{/results}}"
-             n name = singleton "name" name
-             results x = singleton "results" x
-             m = TemplateDictionary empty (results [TemplateDictionary (n "Brandon") empty, TemplateDictionary (n "John") empty])
+         testSubsection
+         testFillK
+         testFillSubTemplate
+         testOutputFilter
 
-         print $ fill y [m]
+testSubsection = do 
+   print $ fill y [m]
+   where y = runOrError "{{#results}}{{name}}, {{/results}}"
+         m = (initTemplateValues <<$ ("results", initTemplateValues <<# ("name", "Juan")) <<$ ("results", initTemplateValues <<# ("name", "Brandon")))
 
+testFillK = do 
+   f <- readFile "k.tmpl" 
+   let p = runOrError f
+   let v = initTemplateValues <<# ("hithere", "Hi There")
+   
+   print $ fill p [v]
+
+testOutputFilter = do 
+   let t = runOrError "whats up {{hello:dots}}"
+       dots s = "..." ++ s ++ "..."
+       v = ((initTemplateValues <<# ("hello", "Brandon")) `addOutputFilter` "dots") dots
+   print $ fill t [v]
+
+testFillSubTemplate = do 
+   f <- readFile "k.tmpl"
+   g <- readFile "g.tmpl"
+   let f'  = runOrError f
+       g'  = runOrError g
+       f'' = Template f' (initTemplateValues <<# ("hithere", "Hi There"))
+       v = initTemplateValues `addSubTemplate` "K" $ f''
+   print $ fill g' [v]
